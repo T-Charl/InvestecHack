417584c79a5857151556b69dc697016001b531ba
Thabani Hlatshwayo <hlatshie01@gmail.com>
2024-10-05 14:27:58 +0200

    Push from Thabani

--- TeachWealth-Backend/app.py
+++ TeachWealth-Backend/app.py
@@ -1,5 +1,20 @@
 from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS
+import google.generativeai as genai
+from flask_socketio import SocketIO
+from dotenv import load_dotenv
 import requests
+import os
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+CORS(app)
+
+
+load_dotenv()
+
+key = os.getenv("GEMINI_API")
 
 app = Flask(__name__)
 
@@ -62,6 +77,7 @@ def get_transactions(account_id):
 
     return jsonify(categorized_transactions)
 
+
 # POST set category limits
 @app.route('/set_limit', methods=['POST'])
 def set_limit():
@@ -74,6 +90,57 @@ def set_limit():
         return jsonify({"message": f"Limit set for {category} category"}), 200
     else:
         return jsonify({"message": "Invalid category"}), 400
+    
+# Set up the API key
+genai.configure(api_key=key)
+
+# Create the model
+generation_config = {
+    "temperature": 1,
+    "top_p": 0.95,
+    "top_k": 64,
+    "max_output_tokens": 8192,
+    "response_mime_type": "text/plain",
+}
+
+# set up the model
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-flash",
+  generation_config=generation_config,
+  system_instruction="\"You are an Investec Transaction Analysis Chatbot provides valuable insights into users' financial habits, helping them identify areas where they may be overspending and offering practical solutions for better money management. For instance, after reviewing the transaction history, the bot might highlight, \"You spent 40% more on dining this month compared to last. Consider setting a weekly dining budget to cut down on excess spending.\" It also offers personalized tips, such as choosing more affordable alternatives for recurring purchases or services. Beyond spending insights, the chatbot delivers tailored recommendations for saving and investing. After identifying areas where users can cut back, it suggests strategies for saving, such as canceling unused subscriptions or adjusting spending habits in certain categories. Based on the savings, the bot proposes Investec’s financial products, like high-yield savings accounts or low-risk bonds, and even provides automated investment advice tailored to individual spending and saving patterns. For long-term financial growth, the chatbot helps users set aside a percentage of monthly savings into flexible investment accounts, integrating Investec’s wealth management services to create a personalized financial growth plan aligned with their goals. You should not ask for transactions at all. Transaction history will be provided through the backend of the application. Interact with the users as if you have the insight that are required. Keep responses short and simple\"",
+)
+# Start a chat session
+chat_session = model.start_chat(history=[], enable_automatic_function_calling=True)
+
+
+# Get the response from the model
+def get_response(input_text):
+
+    response = chat_session.send_message(input_text)
+    return response.text
+
+
+
+@app.route('/chatbot', methods=['POST'])
+def model():
+    try:
+        # Get user input from the request
+        user_input = request.get_json()
+        print(f"\n\nUser input: {user_input}\n\n")
+
+        # Extract the message from the JSON (assuming it's structured as {'message': '...'})
+        message = user_input.get('message')
+        response = get_response(message)
+
+        # Return the assistant message as JSON
+        return jsonify({'message': response})
+
+    except Exception as e:
+        print(f"Error in model: {e}")
+        return jsonify({'error': "An error occurred"}), 500
+
+
+
 
 @app.route('/get_limit/<category>', methods=['GET'])
 def get_limit(category):
@@ -83,4 +150,6 @@ def get_limit(category):
         return jsonify({"message": "Category limit not set or invalid category"}), 400
 
 if __name__ == '__main__':
+    # socketio.run(app, debug=True)
     app.run(debug=True)
+
